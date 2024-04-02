12411aedacc237f15035e30dc04e18c77d98bfe4
Shivani Tanwar <srautanwar04@gmail.com>
2024-04-02 14:31:31 +0530

    some chages

--- Qodit_frontend/src/components/Pages/MoreServices/MoreServices.jsx
+++ Qodit_frontend/src/components/Pages/MoreServices/MoreServices.jsx
@@ -15,7 +15,7 @@ export const MoreServices = (props) => {
       <div className="background-svg">
         <img
           className="background-header"
-          src="img/Services.png"
+          src="img/qoditservice.png"
           width="100%"
           alt=""
         />
